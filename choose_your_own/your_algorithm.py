cd778c350d6bf72f3ff3411adb1d9851fefbfab4
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2015-01-11 17:41:15 -0500

    Attempted K Nearest Neighbors

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -30,9 +30,13 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.neighbors import KNeighborsClassifier
 
+clf = KNeighborsClassifier(n_neighbors=5)
+clf.fit(features_train, labels_train)
 
-
+accuracy = clf.score(features_test, labels_test)
+print "Accuracy of KNN is %f " %accuracy
 
 
 
