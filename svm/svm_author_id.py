81352f65b682abde3937766ccaebb1848507ed12
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2015-01-11 09:44:36 -0500

    Added code to train SVM for Udacity homework

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,6 +26,31 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+CVal = 10000
+clf = SVC(C=CVal, kernel='rbf') #make the classifier
+
+#reducing the number of samples for speed
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100] 
+
+#fit the classifier
+clf.fit(features_train, labels_train) 
+t0=time()
+pred = clf.predict(features_test)
+print "Training time", round(time()-t0,3), "s" 
+
+accuracy = clf.score(features_test, labels_test)
+print "The accuracy of this classifier is %f" %accuracy
+
+idList = [10, 26, 50]
+for id in idList:
+    print "Prediction for %d element is %f" %(id, pred[id])
+
+#number of emails predicted to be Chris
+numChris = sum(pred == 1)
+print "Number of emails predicted to be Chris(1.0) is %d" %numChris
+
 
 #########################################################
 
