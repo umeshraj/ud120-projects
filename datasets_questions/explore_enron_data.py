cb79a4954050aec3fc4b15b05679ba4423297baf
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2015-01-13 21:49:39 -0500

    A nicer way to get POIs by looking at values of dict instead of keys

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-""" 
-    starter code for exploring the Enron dataset (emails + finances) 
+"""
+    starter code for exploring the Enron dataset (emails + finances)
     loads up the dataset (pickled dict of dicts)
 
     the dataset has the form
@@ -12,7 +12,7 @@
     but here's an example to get you started:
 
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
+
 """
 
 import pickle
@@ -28,8 +28,8 @@ numFeatues = len(enron_data[allKeys[1]])
 
 #how many pois are there?
 numPOI = 0
-for key in enron_data.keys():
-    if enron_data[key]['poi'] is True:
-        numPOI +=1
+for value in enron_data.values():
+    if value['poi'] is True:
+        numPOI += 1
+
 print "The number of POIs is %d" %numPOI
-        
