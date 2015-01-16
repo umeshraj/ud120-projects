7ad4d88e767aad3b39c869320da52da4d95c540b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2015-01-16 07:01:43 -0500

    Mini project on regression using finance data

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -23,6 +23,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
+#features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)#, "long_term_incentive"], remove_any_zeroes=True )
 target, features = targetFeatureSplit( data )
 
@@ -30,7 +31,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,11 +39,19 @@ test_color = "b"
 ### please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly
 
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+print "Intercept is %f; Slope is %f" %(reg.intercept_, reg.coef_)
 
+#training score
+trainScore = reg.score(feature_train, target_train)
+print "Score for training: %f" %trainScore
 
+testScore = reg.score(feature_test, target_test)
+print "Score for testing: %f" %testScore
 
-
-
+# regressing bonus with long_term_incentive
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -64,6 +73,10 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="r") 
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
