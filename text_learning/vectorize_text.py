5655fec217d755ff344912c1e1e8984d027db385
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2015-02-01 18:29:47 -0500

    Testing out TfIdf vectorization

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,20 +41,27 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
         temp_counter += 1
-        if temp_counter < 200:
-            path = os.path.join('..', 'tools', path[:-1])
+        if temp_counter < 200e6:
+            path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            tmp = parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            remWordList = ["sara", "shackleton", "chris", "germani", "sshacklensf", "cgermannsf"]
+            for remWord in remWordList:
+                tmp = tmp.replace(remWord,'')
 
             ### append the text to word_data
+            word_data.append(tmp)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if (name == "sara"):
+                from_data.append(0)
+            elif (name == "chris"):
+                from_data.append(1)
 
             email.close()
 
@@ -70,5 +77,14 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 ### in Part 4, do TfIdf vectorization here
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words='english')
+vectorizer.fit_transform(word_data)  # fit to current text data
 
+#getting the features 
+numWords = len(vectorizer.get_feature_names())
+print "number of words is %d" %numWords
 
+#world number 34597
+allFeatures = vectorizer.get_feature_names()
+print "The 34597 featue is %s" %allFeatures[34597]
