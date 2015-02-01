c85ac81ee313086e1e3d8e8884e8116c96475704
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2015-02-01 18:38:26 -0500

    Flying solo on creating training/testing datasets

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -26,5 +26,24 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(features, labels)  # train and test on entire set
+score = clf.score(features, labels)
+
+print "Score on full data is %f" %score
+
+## now we do train/test split
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = \
+train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+score = clf.score(features_test, labels_test)
+print "Score on test data is %f" %score
+
+
+
 
 
