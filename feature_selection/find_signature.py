fd9f1627990ce9a03617f177b1ef2de73e9bc04a
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2015-02-01 18:37:46 -0500

    Exploring words that are significant features

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -6,8 +6,11 @@ numpy.random.seed(42)
 
 
 ### the words (features) and authors (labels), already largely processed
-words_file = "word_data_overfit.pkl" ### like the file you made in the last mini-project 
-authors_file = "email_authors_overfit.pkl"  ### this too
+#words_file = "word_data_overfit.pkl" ### like the file you made in the last mini-project 
+#authors_file = "email_authors_overfit.pkl"  ### this too
+words_file = "../text_learning/your_word_data.pkl" ### like the file you made in the last mini-project 
+authors_file = "../text_learning/your_email_authors.pkl"  ### this too
+
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
 
@@ -34,6 +37,28 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+trainScore = clf.score(features_train, labels_train)
+print "Training score is %f" %trainScore
 
+# important features and nmber of feature
+featImp = clf.feature_importances_
+import numpy as np
+idx = np.argsort(featImp)  # sort and pick the largest value
+largestIdx = idx[-1]
+print "feature number %d, feature value: %f " %(largestIdx, featImp[largestIdx])
 
+## get the feature name that is best discriminating the two emails
+featureNames = vectorizer.get_feature_names()
+maxDiscrimName = featureNames[largestIdx]
+print "the most discriminative word is %s" %maxDiscrimName
 
+#number of words with importance > 0.2
+numImp = sum(featImp >0.2)
+print "Num. words with importance > 0.2 is %d" %numImp
+
+#accuracy of the decisions tree
+testScore = clf.score(features_test, labels_test)
+print "Test score is %f" %testScore
